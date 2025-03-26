52070e7e6d085e9d20f02d13771c37fa84c27ed3
rushjaylen <111437845+rushjaylen@users.noreply.github.com>
2025-03-26 13:51:13 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,15 @@
+document.addEventListener("DOMContentLoaded", () => {
+    console.log("Portfolio Loaded Successfully!");
+});
+
+module.exports = {
+    theme: {
+      extend: {
+        animation: {
+          spinOnce: 'spin 1s linear 1',
+        },
+      },
+    },
+  };
+  
+  
